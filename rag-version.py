344e974105614568e6d20eb4d65bdc114b5f7427
ipan650 <129220740+ipan650@users.noreply.github.com>
2024-01-06 19:53:27 -0800

    Update rag-version.py

--- rag-version.py
+++ rag-version.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 import os
+import time
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 assistant = client.beta.assistants.create(
@@ -8,31 +9,30 @@ assistant = client.beta.assistants.create(
     tools=[{"type": "code_interpreter"}, {"type": "retrieval"}],
     model="gpt-3.5-turbo-1106",
 )
-
 thread = client.beta.threads.create()
-
-
-user_input = input("You: ")
-message = client.beta.threads.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content=user_input,
-)
-
-run = client.beta.threads.runs.create(
-    thread_id=thread.id,
-    assistant_id=assistant.id,
-)
-
-run = client.beta.threads.runs.retrieve(
-    thread_id = thread.id,
-    run_id = run.id,
-)
-
-messages = client.beta.threads.messages.list(
-    thread_id = thread.id,
-)
-print(len(messages.data))
-for message in messages.data:
-    print(message.role + ": " + message.content[0].text.value)
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["quit", "exit", "bye"]:
+        break
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=user_input,
+    )
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+    )
+    while True:
+        run = client.beta.threads.runs.retrieve(
+            thread_id = thread.id,
+            run_id = run.id,
+        )
+        if run.status == "completed":
+            break
+        time.sleep(0.5)
+    messages = client.beta.threads.messages.list(
+        thread_id = thread.id,
+    )
+    print('Assistant: ' + messages.data[0].content[0].text.value)
 
